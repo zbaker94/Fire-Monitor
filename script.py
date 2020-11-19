f684311f071d53fc0fcdd9ca7ef519e927d8b8fc
zbaker94 <zbaker94@gmail.com>
2020-11-18 19:52:46 -0500

    creates settings.json if it does not exist and also gets / writes device id to settings.json if it is not already there

--- script.py
+++ script.py
@@ -1,9 +1,22 @@
 import threading
 import json
 from time import sleep
+import os.path
+from os import path
+
+from getmac import get_mac_address as gma
 
 from google.cloud import firestore
 
+def Merge_Dict(dict1, dict2):
+    if dict1 is not None:
+        if dict2 is not None:
+            return dict1 | dict2
+        else: 
+            return dict1
+    else: 
+        print("cannot merge 2 None values")
+        return None
 
 def listen_document(collection, document):
     db = firestore.Client()
@@ -16,11 +29,14 @@ def listen_document(collection, document):
         for doc in doc_snapshot:
             print(f'Received document snapshot: {doc.id}')
             data = doc.to_dict()
-            
-            if data != json.loads(settings):
+           
+            settings_file = open("settings.json", "r")
+            settings = settings_file.read()
+            settings_dict = json.loads(settings)
+            if data != settings_dict:
                 print("updating settings.json: " + str(data))
                 with open('settings.json', 'w') as fp:
-                    json.dump(data, fp)
+                    json.dump(Merge_Dict(settings_dict, data), fp)
            
         callback_done.set()
 
@@ -30,16 +46,35 @@ def listen_document(collection, document):
     doc_watch = doc_ref.on_snapshot(on_snapshot)
 
 db = firestore.Client()
-### TODO get/generate device id
+### Create settings.json if it doesn't exist
+if not path.exists('settings.json'):
+    try:
+        f = open('settings.json', 'w+')
+        json.dump({}, f, indent = 4)
+        print("Created settings.json")
+        f.close()
+    except FileNotFoundError:
+        print("Could not create or find settings.json")
+### get settings off disk
+settings_file = open("settings.json", "r")
+settings = settings_file.read()
+settings_dict = json.loads(settings)
+print('Found settings.json: ' + str(settings_dict))
+
+### get/generate device id
+device_id = gma()
+
+if device_id not in settings_dict or settings_dict.device_id != device_id:
+    print("Setting device id: " + str(device_id))
+    settings_dict["device_id"] = device_id
+    with open('settings.json', 'w') as fp:
+        json.dump(settings_dict, fp)
 ### TODO check if document exists but if not, create it in `devices` collection
 
 ### listen to changes to device settings
 listen_document('devices', 'GUID')
 listen_delay = 1
 listen_msg = "listening..."
-settings_file = open("settings.json", "r")
-settings = settings_file.read()
-print('Found settings.json: ' + str(settings_file.read()))
 
 while True:
     sleep(listen_delay)
